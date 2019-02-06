28ecb5a6f22c374328b1086722cbdbce06f16f80
Seolhee Kim <seolheekim808@gmail.com>
2019-02-06 14:18:35 -0800

    installed axios and created network request

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import axios from 'axios';
+
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-     <div className="ui container" style={{ marginTop: '10px'}}>
-      <SearchBar />
-     </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID fe61e0f4769944ec05b7fef40eddbc66b93a5ed866db8c526a860ebf04a30b0b'
+      }
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+      </div>
+    );
+  }
+}
+
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,13 @@
+
 import React from 'react';
 
 class SearchBar extends React.Component {
-  // Initializing the state, and it has a single prop called "term" as an empty
-  // string. Every single time a user types inside the input it will update
-  // the term prop with whatever the current value.
-  state = {
-    term: ''
+  state = { term: '' };
+
+  onFormSubmit = event => {
+    event.preventDefault()
+    this.props.onSubmit(this.state.term)
   };
-
-  //this method will get called when user changes the text inside the input.
-  // onInputChange(event) {
-  //   console.log(event.target.value) //<- this will contain the text the user inputs
-  // }
-
-  onFormSubmit(event) {
-    //event.preventDefault() <- this code will keep the browser from submiting the
-    // form automatically and refreshing the page.
-    event.preventDefault() //this is a callback
-
-    console.log(this.state.term)
-  }
 
   render() {
     return (
@@ -39,8 +27,56 @@ class SearchBar extends React.Component {
         </form>
       </div>
     );
-  }
-}
+  };
+};
 
 export default SearchBar;
+
+
+// import React from 'react';
+
+// class SearchBar extends React.Component {
+//   // Initializing the state, and it has a single prop called "term" as an empty
+//   // string. Every single time a user types inside the input it will update
+//   // the term prop with whatever the current value.
+//   state = {
+//     term: ''
+//   };
+
+//   //this method will get called when user changes the text inside the input.
+//   // onInputChange(event) {
+//   //   console.log(event.target.value) //<- this will contain the text the user inputs
+//   // }
+
+//   onFormSubmit = (event) => {
+//     //event.preventDefault() <- this code will keep the browser from submiting the
+//     // form automatically and refreshing the page.
+//     event.preventDefault() //this is a callback
+//     this.props.onSubmit(this.state.term )
+
+//     // console.log(this.state.term)
+//   }
+
+//   render() {
+//     return (
+//       <div className="ui segment">
+//         <form
+//           onSubmit={this.onFormSubmit}
+//           className="ui form"
+//         >
+//           <div className="field">
+//             <label> Image Search </label>
+//             <input
+//               type='text'
+//               onChange={(event) => this.setState({term: event.target.value}) }
+//               value= {this.state.term}
+//             />
+//           </div>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// export default SearchBar;
 
